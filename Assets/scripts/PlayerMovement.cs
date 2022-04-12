4a422f351e6f4ce8c66da8d16e46e3740ab8d8e4
Amine TRABELSI <trabelsi-med-amine@hotmail.fr>
2022-04-12 02:50:14 +0200

    Animation herso
    Creation et premiere animation du joueur

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -7,20 +7,36 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     
     public Rigidbody2D rb;
+    public Animator animator;
     private Vector3 velocity = Vector3.zero;
-
+    public SpriteRenderer spriteRenderer;
     void FixedUpdate()
     {
         float horizontalMovement = Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
         MovePlayer(horizontalMovement);
 
-
+        Flip(rb.velocity.x);
+        float characterVelocity = Mathf.Abs(rb.velocity.x); //renvoyer toujours une valeur positive de la vitesse
+        animator.SetFloat("speed", characterVelocity);
+    
     }
 
     void MovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+
+    void Flip (float _velocity)
+    {
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
     
 
---
+++ Assets/scripts/chargement_jeu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chargement_jeu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+          //Start the coroutine we define below named ChangeAfter2SecondsCoroutine().
+    StartCoroutine(ChangeAfter2SecondsCoroutine());
+    }
+
+IEnumerator ChangeAfter2SecondsCoroutine()
+{
+    //Print the time of when the function is first called.
+    Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+    //yield on a new YieldInstruction that waits for 4 seconds.
+    yield return new WaitForSeconds(4);
+
+
+    //And load the scene
+    Application.LoadLevel("Mission1");  //<-- This is the correct name of the scene
+}
+}
