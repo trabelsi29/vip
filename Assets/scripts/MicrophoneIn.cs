bcf6ef26c3350a46ed6207ae24bd4c433bdf6a83
Amine TRABELSI <trabelsi-med-amine@hotmail.fr>
2022-04-14 02:08:39 +0200

    MJ Mission 1

---
+++ Assets/scripts/MicrophoneIn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneIn : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start("PC-LM1E Audio", true, 10, 44100);
+       
+        audioSource.Play();
+
+        while(!(Microphone.GetPosition(null) > 0)) {}
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {	
+    }
+    
+}
